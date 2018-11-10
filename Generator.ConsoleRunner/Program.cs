56004910664e3f55d1a78d06c72b07cdabf47840
Kaspars Aleksejevs <20342419+kasparsaleksejevs@users.noreply.github.com>
2018-11-10 15:02:02 +0200

    Controller methods reader

--- Generator.ConsoleRunner/Program.cs
+++ Generator.ConsoleRunner/Program.cs
@@ -8,26 +8,39 @@ namespace Generator.ConsoleRunner
         [STAThread]
         static void Main(string[] args)
         {
+
+            Controllers();
+
+
+            Console.WriteLine("=============");
+            Console.ReadKey();
+        }
+
+        static void Controllers()
+        {
+            const string solutionName = "Test";
+            var dte = DteLoader.GetDteBySolutionName(solutionName);
+
+            var processor = new ControllerMethodsReader(dte);
+            processor.Process($@"{solutionName}\TestWeb\Controllers\ReportController.cs");
+
+            Console.WriteLine("====");
+            processor.Process($@"{solutionName}\TestWeb\Controllers\api\ReportController.cs");
+
+        }
+
+        static void Interfaces()
+        {
             const string solutionName = "KnockoutTypeScriptGenerator";
             //const string solutionName = "OldKnockoutTypeScriptGenerator";
 
             var dte = DteLoader.GetDteBySolutionName(solutionName);
 
             var processor = new CodeReader(dte);
-            //processor.ProcessClassFile($@"{solutionName}\Generator.SampleReferencedLibrary\ExampleClass.cs");
-            //processor.ProcessClassFile($@"{solutionName}\Generator.SampleLibrary\SimpleClasses\Sample_Int32.cs");
-
-            //processor.ProcessClassFile($@"{solutionName}\Generator.SampleLibrary\SimpleClasses\enum\Sample_Enum.cs");
             processor.ProcessClassFile($@"{solutionName}\Generator.SampleLibrary\ComplexClasses\Sample_MultipleEnums_MultipleInts.cs");
-            //processor.ProcessClassFile($@"{solutionName}\MyGenerator\Simple\Sample_NullableEnum.cs");
-
-            //var rez = processor.GenerateInterface("Generator.SampleLibrary.ComplexClasses.Sample_MultipleEnums_MultipleInts");
             var rez = processor.GenerateInterface();
+            //var rez = processor.GenerateInterface("Generator.SampleLibrary.ComplexClasses.Sample_MultipleEnums_MultipleInts");
             Console.WriteLine(rez);
-
-
-            Console.WriteLine("=============");
-            Console.ReadKey();
         }
     }
 }
---
+++ KnockoutTypeScriptGenerator/ControllerMethodsReader.cs
@@ -0,0 +1,78 @@
+﻿using EnvDTE;
+using KnockoutTypeScriptGenerator.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KnockoutTypeScriptGenerator
+{
+    public class ControllerMethodsReader
+    {
+        private readonly DTE dte;
+
+        public ControllerMethodsReader(DTE dte)
+        {
+            this.dte = dte;
+        }
+
+        public void Process(string fileName)
+        {
+            var projectItem = this.dte.Solution.FindProjectItem(fileName);
+
+            var count = this.dte.Solution.Projects.Count;
+            var name = this.dte.Solution.FullName;
+
+            foreach (CodeElement element in projectItem.FileCodeModel.CodeElements)
+            {
+                if (element.Kind == vsCMElement.vsCMElementNamespace)
+                {
+                    var cn = (CodeNamespace)element;
+                    foreach (CodeElement member in cn.Members)
+                    {
+                        if (member.Kind == vsCMElement.vsCMElementClass)
+                            this.ProcessClass((CodeClass)member);
+                    }
+                }
+            }
+        }
+
+        private void ProcessClass(CodeClass codeClass)
+        {
+            var funcs = codeClass.Members.OfType<CodeFunction>().Where(m => m.Access == vsCMAccess.vsCMAccessPublic);
+            foreach (var item in funcs)
+            {
+                if (item.FunctionKind != vsCMFunction.vsCMFunctionFunction)
+                    continue;
+                var stuff = "public " + item.Name + "(";
+                var lstParameters = new List<string>();
+                foreach (CodeParameter parameter in item.Parameters)
+                    lstParameters.Add($"{parameter.Type.AsFullName} {parameter.Name}");
+
+                stuff += string.Join(", ", lstParameters);
+                stuff += $") : {item.Type.AsFullName};";
+
+                Console.WriteLine(stuff);
+            }
+        }
+    }
+
+    class GeneratorCodeController : IGeneratorCodeItem
+    {
+        public string Name { get; set; }
+
+        public string Namespace { get; set; }
+
+        public string FullName { get; }
+    }
+
+    class GeneratorCodeControllerMethod
+    {
+        public string Name { get; set; }
+
+        public GeneratorCodeProperty ReturnData { get; set; }
+
+        public List<GeneratorCodeProperty> Parameters { get; set; }
+    }
+
+
+}
